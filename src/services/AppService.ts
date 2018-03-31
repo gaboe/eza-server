cf9588471f8e76990ff637d1ebb131bf680fdc84
gaboe <gabrielecegi@gmail.com>
2018-03-31 23:46:00 +0200

    add shortid

--- src/services/AppService.ts
+++ src/services/AppService.ts
@@ -1,9 +1,9 @@
 import { App, IApp } from "../models/Apps/App";
-
+import * as shortid from "shortid";
 
 const createApp = () => {
     const a: IApp = {
-        description: { name: "TEST" },
+        description: { name: `app-${shortid.generate()}` },
         menuItems: [{ name: "menuItemTest", order: 0 }],
         pages: [{
             name: "page0",

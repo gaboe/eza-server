ab07cd6143aee2c83e596df1f8ca5c13d3a15251
gaboe <gabrielecegi@gmail.com>
2018-03-31 18:32:14 +0200

    improve connection to sql

--- src/services/SqlConfigService.ts
+++
@@ -1,5 +0,0 @@
-const sql = require("mssql");
-
-const connect = () => sql.connect("mssql://app:123@localhost/eza");
-const close = () => sql.close();
-export { connect, close };
--- src/services/SqlQuery.ts
+++ src/services/SqlQuery.ts
@@ -1,12 +1,13 @@
-import { connect, close } from "./SqlConfigService";
+const sql = require("mssql");
 
 const executeQuery = async<From, To>(query: string, map: (sqlEntity: From) => To) => {
-    const pool = await connect();
+    const pool = new sql.ConnectionPool("mssql://app:123@localhost/eza");
+    await pool.connect();
+
     const result: SqlResponse<From> = await pool
         .request()
         .query(query);
 
-    await close();
     const schemas: To[] = result.recordset.map(e => map(e));
 
     return schemas;
